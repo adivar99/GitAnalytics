36ae8d16657bc4c5420e901100cb661641831657
Adithya Vardhan <adivar1999@gmail.com>
2024-08-18 15:46:56 +0530

    Frontend: Remove create project functionality from header

--- frontend/angular-app/src/app/components/header/header.component.ts
+++ frontend/angular-app/src/app/components/header/header.component.ts
@@ -38,7 +38,6 @@ export class HeaderComponent implements OnInit {
     this.auth.currentUser.subscribe(
       (user: User) => {
         this.current_user = user;
-        // this.auth.refresh_projects()
       }
     )
     this.auth.LoggedIn.subscribe(
@@ -46,17 +45,6 @@ export class HeaderComponent implements OnInit {
         this.isLoggedIn = login
       }
     )
-    // this.auth.currentProject.subscribe((proj) => {
-    //   console.log("Current project is: ")
-    //   console.log(proj)
-    //   this.current_proj = proj
-    // })
-    // this.auth.refresh_projects()
-    // let projects = this.auth.get_projects()
-    // console.log("Getting projects: ")
-    // console.log(projects)
-    // this.project_menu = projects
-    // this.current_proj = projects[0]
   }
 
   loginButton() {
@@ -73,54 +61,12 @@ export class HeaderComponent implements OnInit {
     }
   }
 
-  project_menu_placeholder() {
-    if(this.current_proj) {
-      return {
-        'name': this.current_proj.name,
-        'icon': 'down'
-      }
-     } else if (this.current_user?.is_admin) {
-        return {
-          'name': 'Create Project',
-          'icon': 'addcircleoutline'
-        }
-    } else {
-      return {
-        'name': 'Create Project Request',
-        'icon': 'addcircleoutline'
-      }
-    }
-  }
-
-  toggleProjectWindow() {
-    this.isProjectWindowOpened = !this.isProjectWindowOpened;
-  }
-
   processLogout() {
     this.http.postData("/login/logout",{}).subscribe((data) => {
       this.auth.get_current_user()
       this.utils.redirect_page_to('landing')
       this.login_value = 'Get Started'
     })
-  }
-
-  createProject(form: NgForm) {
-    let body = {
-      "title": form.value.name,
-      "description": form.value.description,
-      "company_id": String(this.current_user.company_id)
-    }
-    console.log("Creating project with ");
-    console.log(body)
-    this.http.postData("/project/", body).subscribe((data) => {
-      this.auth.set_project(data)
-      this.auth.refresh_projects()
-    })
-    this.toggleProjectWindow()
-  }
-
-  updateProject(proj: string) {
-    console.log("Updating project", proj)
   }
 
 }
